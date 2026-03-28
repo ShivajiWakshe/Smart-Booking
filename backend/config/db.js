881e925b881309074a465249bb8b1e25215d861a
Shivaji Wakshe <shivajiwakshe2001@gmail.com>
2026-03-28 11:03:28 +0530

    added mongodb uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // ✅ only this
+
     console.log("MongoDB connected ✅");
   } catch (error) {
     console.error("MongoDB connection error ❌", error.message);
-    process.exit(1); // exit if DB connection fails
+    process.exit(1);
   }
 };
 
